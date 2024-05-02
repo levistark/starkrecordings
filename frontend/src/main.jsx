0d1795aee406283582eb96e51c32daf116e3a563
Levi Stark <levi.john.stark@gmail.com>
2024-05-02 14:00:45 +0200

    added react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AutoScrollToTop from './utill/AutoScrollToTop.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AutoScrollToTop />
+    
     <App />
   </React.StrictMode>,
 )
