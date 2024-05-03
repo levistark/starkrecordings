3abbe3c5fdda683b15fa37aea6e779f092289b9e
Levi Stark <levi.john.stark@gmail.com>
2024-05-03 13:31:06 +0200

    fixed mobile navbar and UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,91 +1,82 @@
 import React, { useState, useEffect } from 'react'
 import logo from '../../public/assets/images/logo.webp'
 import logoFallback from '../../public/assets/images/logo.png'
+import logoText from '../../public/assets/images/logo-text.webp'
+import logoTextFallback from '../../public/assets/images/logo-text.png'
+import { Link } from 'react-router-dom'
+import Navbar from './Navbar'
 
 const Header = () => {
     const [scrollState, setScrollState] = useState(0)
     const [lastScrollState, setLastScrollState] = useState(0)
     const body = document.body;
+    const [isOpen, setIsOpen] = useState(false);
+    const [scrollDirection, setScrollDirection] = useState('');
 
+    function toggleMenu() {
+        setIsOpen(!isOpen);
+    }
+    
     useEffect(() => {
-        window.addEventListener('scroll', () => { setScrollState(window.scrollY) })
-
-        return () => {window.removeEventListener('scroll', () => {})}
-    }, []);
+        const handleScroll = () => {
+          setScrollState(window.scrollY);
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
 
     useEffect(() => {
         handleHeaderOnScroll()
     }, [scrollState]);
 
-
     function handleHeaderOnScroll() {
-
         if (window.screen.width > 767) {
-            if (scrollState <= 0) {
-                body.classList.remove("scroll-up");
-                return;
-            }
-            if (scrollState > lastScrollState && !body.classList.contains("scroll-down")) {
-                body.classList.remove("scroll-up");
-                body.classList.add("scroll-down");
-    
-            } else if (scrollState < lastScrollState && body.classList.contains("scroll-down")) {
-                body.classList.remove("scroll-down");
-                body.classList.add("scroll-up");
-            }
-            setLastScrollState(scrollState)
+          if (scrollState <= 0) {
+            setScrollDirection('');
+            return;
+          }
+          if (scrollState > lastScrollState) {
+            setScrollDirection('scroll-down');
+          } else if (scrollState < lastScrollState) {
+            setScrollDirection('scroll-up');
+          }
+          setLastScrollState(scrollState);
         }
-    }
+      }
 
   return (
     <>
-        <header id="header">
-            <nav className="navbar fixed-top">
-                <div className="container-fluid">
+        <header id="header" className={`header ${scrollDirection}`}>
+            <div className="top-header container">
+                <div className='logo'>
                     <picture id="header-logo">     
                         <source srcSet={logo} type="image/webp"/>
                         <img src={logoFallback} alt="Levi Stark with an electric guitar"/> 
-                    </picture>       
-                    <div className='header-nav'>
-                        <a href="#welcome">Welcome</a>
-                        <a href="#production">What I Do</a>
-                        <a href="#about">Aboue Me</a>
-                        <a href="#contact">Contact</a>
-                    </div>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                        <div className="custom-navbar">
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </div>
-                    </button>
-                    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div className="offcanvas-header">
-                            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Stark Recordings</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div className="offcanvas-body">
-                            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Full Song Production</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Premium Song Mixing</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">About Me</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="#">Contact Me</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                    </picture>   
+
+                    <picture id="header-logo-text">     
+                        <source srcSet={logoText} type="image/webp"/>
+                        <img src={logoTextFallback} alt="Levi Stark with an electric guitar"/> 
+                    </picture>   
                 </div>
-            </nav>
+                
+                <nav className='header-nav'>
+                    <Link to='/#welcome'>Welcome</Link>
+                    <Link to='/#production'>What I Do</Link>
+                    <Link to='/#about'>About Me</Link>
+                    <Link to='/#contact'>Contact</Link>
+                </nav>
+
+                <div className="navbar-toggle" onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <Navbar isOpen={isOpen} toggleMenu={toggleMenu}/>
         </header>
     </>
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import logo from '../../public/assets/images/logo.webp'
+import logoFallback from '../../public/assets/images/logo.png'
+import logoText from '../../public/assets/images/logo-text.webp'
+import logoTextFallback from '../../public/assets/images/logo-text.png'
+import { Link } from 'react-router-dom'
+
+const Navbar = ({ isOpen, toggleMenu }) => {
+    
+  return (
+    <div id='navbar'>
+        <div onClick={toggleMenu} className={`flyin-backdrop ${isOpen ? 'showBackdrop' : 'hideBackdrop'}`}></div>
+        <div className={`flyin-menu ${isOpen ? 'showMenu' : 'hideMenu'}`} tabIndex="-1">
+            <div className="flyin-header">
+                <div className='flyin-logo-container'>
+                    <picture id="flyin-logo">     
+                        <source srcSet={logo} type="image/webp"/>
+                        <img src={logoFallback} alt="Levi Stark with an electric guitar"/> 
+                    </picture>   
+                    <picture id="flyin-logo-text">     
+                        <source srcSet={logoText} type="image/webp"/>
+                        <img src={logoTextFallback} alt="Levi Stark with an electric guitar"/> 
+                    </picture>
+                </div>
+                    
+                <button onClick={toggleMenu} type="button" className="btn-close"></button>
+            </div>
+            <div className="flyin-body">
+                <nav className="flyin-nav">
+                    <Link to="/" aria-current="page">Home</Link>
+                    <Link to="/#production">Production</Link>
+                    <Link to="/#mixing">Mixing</Link>
+                    <Link to="/#contact">Contact</Link>
+                    <Link to="/#about">About me</Link>
+                </nav>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
