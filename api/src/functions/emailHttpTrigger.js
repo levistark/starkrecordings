c4dbb4e8d53d7677e9c33b627b725cab65658895
Levi Stark <levi.john.stark@gmail.com>
2024-05-10 13:34:50 +0200

    made changes to the package.json files

--- api/src/functions/emailHttpTrigger.js
+++ api/src/functions/emailHttpTrigger.js
@@ -1,27 +1,29 @@
 const { app } = require('@azure/functions');
-const { ServiceBusClient } = require("@azure/service-bus");
+//const { ServiceBusClient } = require("@azure/service-bus");
 
 app.http('emailHttpTrigger', {
     methods: ['POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+        return;
+
         const email = await request.text()
+        
+        // if (email !== "" && email !== null) {
+        //     const sbConnectionString = process.env['AzureServiceBus']
+        //     const sbClient = new ServiceBusClient(sbConnectionString);
+        //     const queueName = 'email-optin'
+        //     const sender = sbClient.createSender(queueName);
+        //     const message = { body: email }
 
-        if (email !== "" && email !== null) {
-            const sbConnectionString = process.env['AzureServiceBus']
-            const sbClient = new ServiceBusClient(sbConnectionString);
-            const queueName = 'email-optin'
-            const sender = sbClient.createSender(queueName);
-            const message = { body: email }
-
-            try {
-                await sender.sendMessages(message);
-                await sender.close();
-            } catch (error) {
-                console.error(`An error occurred: ${error.message}`);
-            } finally {
-                await sbClient.close();
-            }
-        }
+        //     try {
+        //         await sender.sendMessages(message);
+        //         await sender.close();
+        //     } catch (error) {
+        //         console.error(`An error occurred: ${error.message}`);
+        //     } finally {
+        //         await sbClient.close();
+        //     }
+        // }
     }
 });
