1f4b93376b63a785462c4cae1f373231eed20ade
Levi Stark <levi.john.stark@gmail.com>
2024-06-04 20:53:40 +0200

    fixed errors on playlist blog post

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,8 +14,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<App/>}/>
         <Route path='/post' element={<BlogPosts/>}/>
-        <Route path='/post/playlists2023' element={<BlogArticle2023/>}/>
-        <Route path='/post/playlists2024' element={<BlogArticle2024/>}/>
+        <Route path='/posts/playlists2023' element={<BlogArticle2023/>}/>
+        <Route path='/posts/playlists2024' element={<BlogArticle2024/>}/>
         <Route path='/playlists' element={<BlogArticle2024/>}/>
       </Routes>
     </BrowserRouter>
--- frontend/src/pages/blog/articles/BlogArticle2024.jsx
+++ frontend/src/pages/blog/articles/BlogArticle2024.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import Header from '../../../components/Header'
 import Footer from '../../../components/Footer'
 import Popup from '../../../components/Popup'
